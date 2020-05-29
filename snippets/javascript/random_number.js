1d927d2fee1bba7dee38761ff72eaa8010826405
Zack Pace <uiysgamer@gmail.com>
2020-05-29 13:34:35 -0600

    Add docs, documentate random_number

--- snippets/javascript/random_number.js
+++ snippets/javascript/random_number.js
@@ -1,5 +1,5 @@
 function randomNumber(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
 
 console.log(randomNumber(100));
