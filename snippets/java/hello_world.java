7cfa98618df440830855c910d4043bf8d6fe0b10
Zack Pace <uiysgamer@gmail.com>
2020-05-29 16:23:38 -0600

    Remove unnecessary spaces

--- snippets/java/hello_world.java
+++ snippets/java/hello_world.java
@@ -1,11 +1,7 @@
 package main;
 
 class Main {
-
-  public static void main(String[] args) {
-
-    System.out.println("Hello, world!");
-
-  }
-
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
